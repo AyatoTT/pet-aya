e67dc761be2bac7e1b4f60f991b8fcb61776a19c
AyatoTT <vlad.tyurin.2003@mail.ru>
2024-02-01 04:54:10 +0300

    initial comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/home/home";
+import Messages from "./pages/messages/messages";
+import Orders from "./pages/orders/orders";
+
+
+function App(){
+    return(
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/messages" element={<Messages />} />
+                <Route path="/orders" element={<Orders />} />
+
+            </Routes>
+
+        </div>
+    );
+
+
 }
 
-export default App;
+export default App
